648a7c1d4301d890539e9dc4fdd59c5036bc8dc9
Nguyen Duc Quy <quynd@tech.est-rouge.com>
2022-06-01 17:50:16 +0700

    Update user properties and fixed build error

--- common/src/main/java/com/module/jetpack/compose/common/domain/User.kt
+++ common/src/main/java/com/module/jetpack/compose/common/domain/User.kt
@@ -1,7 +1,36 @@
 package com.module.jetpack.compose.common.domain
 
+import java.util.Date
+
 interface User {
-    val userId: String
-    val userName: String
-    val userImageUrl: String
+    val id: Long
+    val login: String
+    val nodeId: String
+    val avatarUrl: String
+    val gravatarId: String
+    val url: String
+    val htmlUrl: String
+    val followersUrl: String
+    val followingUrl: String
+    val gistsUrl: String
+    val starredUrl: String
+    val subscriptionsUrl: String
+    val organizationsUrl: String
+    val reposUrl: String
+    val eventsUrl: String
+    val receivedEventsUrl: String
+    val type: String
+    val siteAdmin: Boolean
+    val score: Float
+    val name: String
+    val company: String
+    val blog: String
+    val location: String
+    val email: String
+    val hireable: String
+    val bio: String
+    val publicRepos: Int
+    val publicGists: Int
+    val followers: Int
+    val following: Int
 }
